08bcf33092f83adb8328e92822702476710674a6
Hassan2bit <hassanamiri.ai@gmail.com>
2026-05-10 01:11:42 +0100

    feat: create a token generator function

---
+++ src/utils/token_generator.utils.js
@@ -0,0 +1,11 @@
+import "jsonwebtoken";
+export const generateToken = (payload) => {
+  return jwt.sign(
+    {
+      id: payload._id,
+      username: payload.username,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+};
