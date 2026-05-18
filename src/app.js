bf99de413386838997ba0cf4d2eaf064a39df3eb
Hassan2bit <hassanamiri.ai@gmail.com>
2026-05-18 10:17:13 +0100

    setup express app for cors, global error handler, health -check etc

---
+++ src/app.js
@@ -0,0 +1,81 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+/*
+cors setup
+*/
+const productionOrigin = process.env.FRONTEND_URL;
+const allowedOrigin = (process.env.APP_ENV = "production"
+  ? productionOrigin
+  : [
+      "http://localhost:3000",
+      "http://localhost:5500",
+      "http://localhost:5173",
+      "http://localhost:4173",
+      "http://127.0.0.1:3000",
+      "http://127.0.0.1:5173",
+    ]);
+
+app.use(
+  cors({
+    origin: allowedOrigin,
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.use(express.json());
+/*
+health check 
+*/
+app.get("/health", (req, res) => {
+  return res.status(200).json({ success: true, message: "healthy" });
+});
+
+/*
+rount mounts
+*/
+
+/*
+global error handler
+*/
+app.use((err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Internal Server Error";
+
+  // 1. Handle Mongoose/MongoDB Validation Errors
+  if (err.name === "ValidationError") {
+    statusCode = 400;
+    message = Object.values(err.errors)
+      .map((val) => val.message)
+      .join(", ");
+  }
+
+  // 2. Handle MongoDB Duplicate Key (e.g., Email already exists)
+  if (err.code === 11000) {
+    const field = Object.keys(err.keyValue)[0];
+    statusCode = 409;
+    message = `${field} already exists.`;
+  }
+
+  // 3. Handle JWT Errors (Categorized by name)
+  if (err.name === "JsonWebTokenError") {
+    statusCode = 401;
+    message = "Invalid token. Please log in again.";
+  }
+
+  if (err.name === "TokenExpiredError") {
+    statusCode = 401;
+    message = "Your token has expired.";
+  }
+
+  return res.status(statusCode).json({
+    success: false,
+    message: message,
+    stack: process.env.APP_ENV === "development" ? err.stack : undefined,
+  });
+});
+
+export default app;
